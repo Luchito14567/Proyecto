f71f07f2df14dc3f5562d2eb420bbf65050d8c15
Luchito14567 <luchito14567@gmail.com>
2024-12-08 23:08:02 -0300

    proyecto

---
+++ src/main/java/com/lucio/tp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lucio.tp.repository;
+
+
+import com.lucio.tp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
